e02b68b4354678ac5f4b81dfc04bd366237de62c
Sherri Odribege <sodribege@caifunds.com>
2022-09-23 14:22:57 -0700

    Rewrite function so that it's background colour that's changed, and not background of button

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
-
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,18 +15,19 @@ public class MainActivity extends AppCompatActivity {
     private Button apiVersionButton;
     private Button serialNumber;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ConstraintLayout mainLayout;
 
+        mainLayout = findViewById(R.id.mainLayout);
         changeBackgroundBtn = findViewById(R.id.changeBackgroundBtn);
         Random rnd = new Random();
 
         changeBackgroundBtn.setOnClickListener(v -> {
             int colour = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
-            changeBackgroundBtn.setBackgroundColor(colour);
+            mainLayout.setBackgroundColor(colour);
             Toast.makeText(MainActivity.this, "New Test", Toast.LENGTH_LONG).show();
         });
     }
