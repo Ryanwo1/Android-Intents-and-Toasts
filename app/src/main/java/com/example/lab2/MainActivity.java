931fb356ae19fff0bad289594a35c60ca91868b5
Ryan Odribege <rodribege@my.bcit.ca>
2022-09-24 19:18:39 -0700

    implement toast feature

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -2,11 +2,12 @@ package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import java.util.Random;
 
@@ -36,8 +37,26 @@ public class MainActivity extends AppCompatActivity {
         textToSpeechBtn.setOnClickListener(v -> {
             Intent intent = new Intent(this, SpeakActivity.class);
             startActivity(intent);
-            Toast.makeText(MainActivity.this, "TTS Test", Toast.LENGTH_LONG).show();
+
 
         });
+
+        apiVersionButton = findViewById(R.id.apiVersionBtn);
+        apiVersionButton.setOnClickListener(v -> {
+            String manufacturer = android.os.Build.MANUFACTURER;
+            String model = android.os.Build.MODEL;
+            int version = android.os.Build.VERSION.SDK_INT;
+            String versionRelease = android.os.Build.VERSION.RELEASE;
+            String messageText = " manufacturer " + manufacturer
+                    + " \n model " + model
+                    + " \n version " + version
+                    + " \n versionRelease " + versionRelease;
+
+           Toast toast = Toast.makeText(MainActivity.this, messageText, Toast.LENGTH_LONG);
+           TextView apiView = toast.getView().findViewById(android.R.id.message);
+           apiView.setTextColor(Color.RED);
+           toast.show();
+        });
     }
+
 }
